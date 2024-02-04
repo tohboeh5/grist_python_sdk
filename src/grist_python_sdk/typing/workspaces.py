41d5a40957f7460eb93bcce4650e3faea0cb58bf
tohboeh5 <>
2024-02-04 17:14:19 +0900

    create workspaces

---
+++ src/grist_python_sdk/typing/workspaces.py
@@ -0,0 +1,25 @@
+from typing import List, Literal, Optional, TypedDict
+
+from grist_python_sdk.typing.orgs import UserInfo
+
+Access = Literal["owners", "editors", "viewers"]
+
+
+class DocumentInfo(TypedDict):
+    id: str
+    name: str
+    access: Access
+    isPinned: bool
+    urlId: Optional[str]
+
+
+class WorkspaceInfoRequired(TypedDict):
+    id: int | str
+    name: str
+    owner: UserInfo
+    access: Access
+    docs: List[DocumentInfo]
+
+
+class WorkspaceInfo(WorkspaceInfoRequired):
+    orgDomain: Optional[str]
